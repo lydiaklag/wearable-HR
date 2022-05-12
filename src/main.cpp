11f7a284afba82ffac14a807a4ea66fc5a44145d
lydiaklag <lydiaklag@gmail.com>
2022-05-12 19:41:08 +0300

    update12may22

---
+++ src/main.cpp
@@ -0,0 +1,426 @@
+#include "SPI.h"
+#include "Wire.h"
+#include "max30105.h"
+#include "IIRFilter.h"
+#include <Arduino.h>
+
+MAX30105 Sensor;
+
+//***Define signal parameters
+int samp_freq=25; //for each led
+const int Num_Samples = 100;  //it stores 4 sec 
+
+uint32_t gr_buffer[Num_Samples];
+int filtered_gr_buffer[Num_Samples];
+int ma_gr_buffer[Num_Samples];
+
+const int points_pr=4;
+float PR[points_pr];
+float Pulse_Rate_next=0, Pulse_Rate_previous=70;
+int HR;
+
+static int taskCore = 1;
+static int taskCore_sp = 0;
+
+//***Butterworth band-pass (0.5Hz-5Hz) 2nd order
+const double b[] = {0.175087643672101,0,-0.350175287344202,0,0.175087643672101};
+const double a[] = {1,-2.299055356038497,1.967497759984451,-0.874805556449481,0.219653983913695};
+
+IIRFilter f(b, a);
+double filtered_gr=0;
+
+int Moving_Average_Num = 2;
+int Num_Points = 2*Moving_Average_Num+1;  //***5-point moving average filter
+int Sum_Points;
+void *ax;
+int Sampling_Time= 2400; //2400ms = 4s
+int flag_unplugged = 0; //0 means unplugged
+
+SemaphoreHandle_t baton;
+
+//regarding SpO2, initialising global variables
+const int points_spo2 = 4; 
+double SpO2_dc_ir[points_spo2]; //no need to initialise the arrays, that is done inside the SpO2 func
+double SpO2_ac_ir[points_spo2];
+double SpO2_dc_red[points_spo2];
+double SpO2_ac_red[points_spo2];
+double Oxy[points_spo2]; 
+int ma_ir2_buffer[Num_Samples];
+int ma_red_buffer[Num_Samples];
+double SpO2_next; //maybe double
+double SpO2_previous = 99;
+double SpO2;
+//declaring functions
+void iir_ma_filter_for_hr();
+int Find_Peak(int p1, int p2, int *x);
+int Find_Mean(int p1, int p2, int *x);
+int find_min_negative(int p1, int p2, int *x);
+void convert_signal_to_positive(int p1, int p2, int *x, int point);
+void ComputeHeartRate();
+int readSamples();
+void setup();
+void loopHR(void * pvParameter);
+void loop();
+void ComputeSpO2(void * pvParameter);
+
+void setup(){
+  
+  Serial.begin(9600);
+ 
+  // Initialize sensor
+  if (Sensor.begin() == false)
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  baton = xSemaphoreCreateMutex();
+  byte ledBrightness = 0xDF; //Options: 0=Off to 255=50mA  --> DF=~44mA
+  byte sampleAverage = 2;    //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 3;          //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  int sampleRate = 200;      //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411;      //Options: 69, 118, 215, 411
+  int adcRange = 2048;       //Options: 2048, 4096, 8192, 16384
+
+  Sensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  Sensor.setPulseAmplitudeRed(0xFF);  //if the value was 0, here we basically turn off the red LED, so green and IR LEDs are active
+  //now that I changed the value to 0xFF it means that all the LEDs are on at the same time 
+  xTaskCreatePinnedToCore(
+  //xTaskCreate(  
+    loopHR,
+    "loop",
+    10000,
+    NULL,
+    1,
+    NULL,
+    taskCore
+  );   
+  
+  xTaskCreatePinnedToCore(
+  //xTaskCreate(  
+    ComputeSpO2,
+    "loop",
+    10000,
+    NULL,
+    1,
+    NULL,
+    taskCore_sp
+  );
+  
+}
+
+
+void loopHR(void * pvParameter) {
+  for(;;){
+    xSemaphoreTake( baton, portMAX_DELAY);
+    int flag=readSamples();
+    Serial.println();
+    Serial.println("----------------------------------------------------------------------------------------------------");
+    if (flag){          //***if sensor reads real data
+      
+      iir_ma_filter_for_hr();
+      int neg=find_min_negative(Moving_Average_Num, Num_Samples - Moving_Average_Num, ma_gr_buffer);
+      convert_signal_to_positive(Moving_Average_Num, Num_Samples - Moving_Average_Num, ma_gr_buffer, neg);
+      ComputeHeartRate();
+      
+      Serial.print("NEW DATA--> ");
+      Serial.print("HR: ");
+      Serial.print(HR);
+      Serial.println();
+      flag_unplugged = 1; 
+    }
+    else{                 //***else if sensor is unplugged
+      flag_unplugged = 0; 
+      HR = 0; 
+      Serial.print("HR : unplugged"); 
+      Serial.println();
+    }
+    //not sure about reseting the flag_unplugged to 0, just trial and error 
+    //semaphores will fix the sunchronisation issue
+    //flag_unplugged = 0;
+    xSemaphoreGive( baton );
+    delay(50);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(10);
+  //here I will put the info for the upload to firebase 
+  
+}
+
+void iir_ma_filter_for_hr(){
+  
+  for (int i=0;i<Num_Samples;i++){
+    
+    filtered_gr = f.filter(double(gr_buffer[i]));
+    filtered_gr_buffer[i] = round(filtered_gr);
+
+  }
+
+  for (int i= Moving_Average_Num;i<Num_Samples-Moving_Average_Num;i++){
+      Sum_Points= 0;
+      for( int k =0; k < Num_Points; k++){   
+        Sum_Points = Sum_Points + filtered_gr_buffer[i-Moving_Average_Num+k]; 
+      }    
+      ma_gr_buffer[i] = Sum_Points/Num_Points; 
+  }
+  
+}
+
+int Find_Peak(int p1, int p2, int *x){
+  int Peak_Magnitude = 0;
+  for (int m = p1; m < p2; m++){
+      if(Peak_Magnitude < x[m]){
+        Peak_Magnitude = x[m];
+      }
+  }
+  return Peak_Magnitude;
+}
+
+int Find_Mean(int p1, int p2, int *x){
+  int Mean = 0;
+  int c = 0;
+  for (int m = p1; m < p2; m++){
+      if (x[m]>0){
+        Mean = Mean+x[m];
+        c++;
+      }
+  }
+  if (c>0){
+    return Mean/c;
+  }else{
+    return 1;
+  }
+}
+
+int find_min_negative(int p1, int p2, int *x){
+  int min_magnitude = 0;
+  for (int m = p1; m < p2; m++)
+  {
+    if (min_magnitude > x[m])
+    {
+      min_magnitude = x[m];
+    }
+  }
+  return min_magnitude;
+}
+
+void convert_signal_to_positive(int p1, int p2, int *x, int point){
+  for (int m = p1; m < p2; m++)
+  {
+     x[m]=x[m]-point;
+  }
+}
+
+void ComputeHeartRate(){
+  
+  int Mean_Magnitude =Find_Mean(Moving_Average_Num, Num_Samples - Moving_Average_Num, ma_gr_buffer);
+  
+  //***detect successive peaks and compute PR
+  for (int i = 0; i < points_pr; i++){
+     PR[i] = 0;
+  }
+  int  Peak = 0;
+  int Index = 0;
+  int p=0;
+
+    
+  for (int j = Moving_Average_Num; j < Num_Samples-Moving_Average_Num; j++){
+      //***Find first peak
+      
+      if(ma_gr_buffer[j] > ma_gr_buffer[j-1] && ma_gr_buffer[j] > ma_gr_buffer[j+1] && ma_gr_buffer[j] > Mean_Magnitude && Peak == 0){
+         Peak = ma_gr_buffer[j];
+         Index = j; 
+      }
+      
+      //***Search for next peak 
+      
+      if(Peak > 0 ){
+       if(ma_gr_buffer[j] > ma_gr_buffer[j-1] && ma_gr_buffer[j] > ma_gr_buffer[j+1] && ma_gr_buffer[j] > Mean_Magnitude){
+        float d=j-Index;
+        float pulse=(float)samp_freq*60/d; //bpm for each PEAK interval
+        PR[p]=pulse; 
+        p++;
+        p %= points_pr; //Wrap variable
+        Peak = ma_gr_buffer[j];
+        Index = j;
+       } 
+      } 
+  } 
+
+  float sum=0;
+  int c=0;
+  for (int i=0; i< points_pr; i++){
+    if (PR[i]!=0 && PR[i]!=INFINITY){
+       sum=sum+PR[i];
+       c=c+1;
+    }      
+  }
+
+  if (c!=0){
+      Pulse_Rate_next=sum/c;
+    if(Pulse_Rate_next > 40 && Pulse_Rate_next < 200){
+      if (Pulse_Rate_next-Pulse_Rate_previous>=5){
+        Pulse_Rate_next=Pulse_Rate_previous+1;
+      }
+      if (Pulse_Rate_next-Pulse_Rate_previous<=-5){
+        Pulse_Rate_next=Pulse_Rate_previous-1;
+      }
+      Pulse_Rate_previous=Pulse_Rate_next;
+    }
+    else{
+      Pulse_Rate_next=Pulse_Rate_previous;
+    }
+  }else{
+    Pulse_Rate_next=Pulse_Rate_previous;
+  }
+
+  HR= int(round(Pulse_Rate_next));
+
+}
+
+int readSamples(){
+ 
+  int flag=1;
+  
+  for (uint32_t i=0;i<Num_Samples;i++){ 
+    //read max30105
+    gr_buffer[i] = Sensor.getGreen();
+    //Sensor.nextSample();
+
+    if (gr_buffer[i]<10000){
+      flag=0;
+    }
+    delay(40);
+  }
+
+  return flag;
+}
+
+void ComputeSpO2(void * pvParameter){
+   //need to initialise all the variables for SpO2, make them global
+for(;;){ //we need this huge endless loop, for the FreeRTOS task, requirement
+    xSemaphoreTake( baton, portMAX_DELAY);
+    if (flag_unplugged = 1) {
+      int mean_ir = Find_Mean(0, Num_Samples, ma_ir2_buffer);
+      int mean_red = Find_Mean(0, Num_Samples, ma_red_buffer);
+      
+      //***detect successive peaks and mins
+      for (int i = 0; i < points_spo2; i++){
+        SpO2_dc_ir[i] = 0;
+        SpO2_ac_ir[i]=0;
+        SpO2_dc_red[i] = 0;
+        SpO2_ac_red[i]=0;
+        Oxy[i]=0;
+      }
+
+      int p_ir=0;
+      int p_red=0;
+      int peak_ir=0;
+      int peak_red=0;
+      int m_ir=0;
+      int m_red=0;
+    
+      for (int j = 101; j < 350; j++){
+        //***Find peaks and means for IR
+        if(ma_ir2_buffer[j] > ma_ir2_buffer[j-1] && ma_ir2_buffer[j] > ma_ir2_buffer[j+1] && ma_ir2_buffer[j] > mean_ir && peak_ir==0){
+        
+          /*Serial.print("peak  ir: ");
+          Serial.print(ma_ir2_buffer[j]);
+          Serial.println();*/
+          SpO2_dc_ir[p_ir]=ma_ir2_buffer[j]; 
+          p_ir++;
+          p_ir %= points_spo2; //Wrap variable
+          peak_ir=1;
+        }
+        
+        if(ma_ir2_buffer[j] < ma_ir2_buffer[j-1] && ma_ir2_buffer[j] < ma_ir2_buffer[j+1] && ma_ir2_buffer[j] < mean_ir && peak_ir==1){
+          /*Serial.print("min  ir: ");
+          Serial.print(ma_ir2_buffer[j]);
+          Serial.println();*/
+          SpO2_ac_ir[m_ir]=SpO2_dc_ir[p_ir-1]-ma_ir2_buffer[j]; 
+          m_ir++;
+          m_ir %= points_spo2; //Wrap variable
+          peak_ir=0;
+        }
+
+        //***Find peaks and means for RED
+        if(ma_red_buffer[j] > ma_red_buffer[j-1] && ma_red_buffer[j] > ma_red_buffer[j+1] && ma_red_buffer[j] > mean_red && peak_red==0){
+        
+          SpO2_dc_red[p_red]=ma_red_buffer[j]; 
+          p_red++;
+          p_red %= points_spo2; //Wrap variable
+          peak_red=1;
+        }
+
+        if(ma_red_buffer[j] < ma_red_buffer[j-1] && ma_red_buffer[j] < ma_red_buffer[j+1] && ma_red_buffer[j] < mean_red && peak_red==1){
+        
+          SpO2_ac_red[m_red]=SpO2_dc_red[p_red-1]-ma_red_buffer[j]; 
+          m_red++;
+          m_red %= points_spo2; //Wrap variable
+          peak_red=0;
+        }
+      }
+      
+
+
+      for (int i=1; i< points_spo2; i++){
+
+        if (SpO2_ac_ir[i]!=0 && SpO2_ac_red[i]!=0){
+          float R =(float(SpO2_ac_red[i])/float(SpO2_dc_red[i]))/(float(SpO2_ac_ir[i])/float(SpO2_dc_ir[i]));
+          Oxy[i]=-45.060*R*R + 30.354*R + 94.845;
+          // Serial.print("oxy: ");
+          // Serial.print(Oxy[i]);
+          // Serial.println();
+        }   
+            
+      }
+
+      float sumSP=0;
+      int cSP=0;
+      
+      for (int i=1; i< points_spo2; i++){
+
+        if (Oxy[i]>84 && Oxy[i]<=100){
+          sumSP=sumSP+Oxy[i];
+          cSP=cSP+1;
+        }   
+      }
+    
+      if(cSP>0){
+        SpO2_next=sumSP/cSP;
+      }
+      
+      // Serial.print("SpO2 predicted: ");
+      // Serial.print(SpO2_next);
+      // Serial.println();
+
+      if(SpO2_next > 84 && SpO2_next <= 100){  
+        if (SpO2_next-SpO2_previous<-4){
+          SpO2_next=SpO2_previous;
+        }
+        if (SpO2_next-SpO2_previous<-3){
+          SpO2_next=SpO2_previous-1;
+        }
+        SpO2_previous=SpO2_next;
+      }else{
+        SpO2_next=SpO2_previous;
+      }
+      //Maria mou, einai okay pou deixnw to SpO2? 'h mhpws na deixnw to SpO2_next? (einai prediction, alla mhpws einai pio accurate?)
+      SpO2=int(round(SpO2_next));
+      if (flag_unplugged == 0 || HR == 0){ //if unplugged show this 
+        //SpO2 = 0; //this is probably not needed
+        Serial.println("SpO2 : unplugged");
+      }else {
+        Serial.print("SpO2 : ");
+        Serial.print(SpO2);
+        Serial.println();
+      }
+      vTaskDelay(Sampling_Time / portTICK_PERIOD_MS);
+    }
+    xSemaphoreGive( baton );
+    delay(50);
+  }
+}
+
